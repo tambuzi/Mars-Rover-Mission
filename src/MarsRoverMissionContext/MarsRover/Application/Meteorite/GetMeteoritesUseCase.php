7ce5b8388b617780bb18bbb512e950f1afc5a50a
tambuzi <alefernandez1997@hotmail.com>
2022-02-26 13:53:40 +0100

    fix index git

---
+++ src/MarsRoverMissionContext/MarsRover/Application/Meteorite/GetMeteoritesUseCase.php
@@ -0,0 +1,22 @@
+<?php
+
+use Laraveltip\MarsRoverMisionContext\MarsRover\Application\Meteorite\CreateMeteoriteUseCase\CreateMeteoriteUseCase;
+use Laraveltip\MarsRoverMisionContext\MarsRover\Domain\Meteorite\MeteoriteCreator\MeteoriteCreator;
+use Laraveltip\MarsRoverMisionContext\MarsRover\Domain\Meteorite\MeteoriteRepository\MeteoriteRepository;
+use Laraveltip\MarsRoverMisionContext\MarsRover\Domain\Meteorite\MeteoriteCollection\MeteoriteCollection;
+
+class GetMeteoritesUseCase
+{
+
+    private array $meteorites;
+
+
+    public function execute()
+    {for ($i = 0; $i <= random_int(0, 30); $i++) {
+
+        $meteorite = new CreateMeteoriteUseCase(new MeteoriteCreator(new MeteoriteRepository()));
+        array_push($meteorites, $meteorite->execute());
+    }
+    return MeteoriteCollection::create($meteorites);
+    }
+}
---
+++ src/MarsRoverMissionContext/MarsRover/Infrastructure/CreateRoverController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Laraveltip\MarsRoverMisionContext\MarsRover\Infrastructure\CreateRoverController;
+
+use Laraveltip\MarsRoverMisionContext\MarsRover\Application\Rover\CreateRoverUseCase\CreateRoverUseCase;
+use Laraveltip\MarsRoverMisionContext\MarsRover\Application\Rover\RoverDTO\RoverDTO;
+use Laraveltip\MarsRoverMisionContext\MarsRover\Domain\Rover\RoverCreator\RoverCreator;
+use Laraveltip\MarsRoverMisionContext\MarsRover\Domain\Rover\RoverRepository\RoverRepository;
+use Exception;
+
+class CreateRoverController
+{
+    private RoverDTO $roverDTO;
+    private CreateRoverUseCase $rover;
+
+    public function create(int $startPositionX, int $startPositionY, string $direction)
+    {
+        try {
+            $this->roverDTO = new RoverDTO(rand(), $startPositionX, $startPositionY, $direction);
+
+            $this->rover = new CreateRoverUseCase(new RoverCreator(new RoverRepository));
+            return $this->rover;
+        } catch (Exception $error) {
+            return $error;
+        }
+    }
+}
