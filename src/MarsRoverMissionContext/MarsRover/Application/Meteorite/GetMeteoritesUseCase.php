869d5806daa196081fe1dc47eafc4f041d86501d
tambuzi <alefernandez1997@hotmail.com>
2022-02-27 15:36:37 +0100

    fix directories

--- src/MarsRoverMissionContext/MarsRover/Application/Meteorite/GetMeteoritesUseCase.php
+++ src/MarsRoverMissionContext/MarsRover/Application/Meteorite/GetMeteoritesUseCase.php
@@ -1,10 +1,10 @@
 <?php
-namespace Laraveltip\MarsRoverMisionContext\MarsRover\Application\Meteorite\GetMeteoritesUseCase;
+namespace Laraveltip\MarsRoverMisionContext\MarsRover\Application\Meteorite;
 
-use Laraveltip\MarsRoverMisionContext\MarsRover\Application\Meteorite\CreateMeteoriteUseCase\CreateMeteoriteUseCase;
-use Laraveltip\MarsRoverMisionContext\MarsRover\Domain\Meteorite\MeteoriteCreator\MeteoriteCreator;
-use Laraveltip\MarsRoverMisionContext\MarsRover\Domain\Meteorite\MeteoriteRepository\MeteoriteRepository;
-use Laraveltip\MarsRoverMisionContext\MarsRover\Domain\Meteorite\MeteoriteCollection\MeteoriteCollection;
+use Laraveltip\MarsRoverMisionContext\MarsRover\Application\Meteorite\CreateMeteoriteUseCase;
+use Laraveltip\MarsRoverMisionContext\MarsRover\Domain\Meteorite\MeteoriteCreator;
+use Laraveltip\MarsRoverMisionContext\MarsRover\Domain\Meteorite\MeteoriteRepository;
+use Laraveltip\MarsRoverMisionContext\MarsRover\Domain\Meteorite\MeteoriteCollection;
 
 class GetMeteoritesUseCase
 {
@@ -13,7 +13,11 @@ class GetMeteoritesUseCase
 
 
     public function execute()
-    {
-        
+    {for ($i = 0; $i <= random_int(0, 30); $i++) {
+
+        $meteorite = new CreateMeteoriteUseCase(new MeteoriteCreator(new MeteoriteRepository()));
+        array_push($meteorites, $meteorite->execute());
+    }
+    return MeteoriteCollection::create($meteorites);
     }
 }
